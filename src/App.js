7f3549fedd0824ecf01917e41811beb51776ae4e
EYE MOTION <eyemotiongamestudio@gmail.com>
2020-04-08 01:05:58 +0400

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useBookSearch from './useBookSearch'
 
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState('1')
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  useBookSearch(query, setPageNumber)
   return (
-    <div className="App">
+    <div>
+      <input type="text" onChange={handleSearch}/>
+      <div>Title</div>
+      <div>Title</div>
+      
+      <div>Title</div>
+      <div>Title</div>
+      <div>Loading ...</div>
+      <div>Error</div>
     </div>
   );
 }
